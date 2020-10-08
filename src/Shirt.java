d8f5312fab6690ebf28dad91bf5fefd8f2831272
Rifleborn <49074281+Rifleborn@users.noreply.github.com>
2020-10-08 20:46:05 +0300

    Update Shirt.java

--- src/Shirt.java
+++ src/Shirt.java
@@ -8,19 +8,27 @@
  *
  * @author User
  */
+import java.util.Scanner;
 public class Shirt {
-  public int shirtID = 0; // стандартне значення номера моделі сорочки
-  public String description = "-description required-"; // стандартний опис сорочки
+  public int shirtID = 1; // стандартне значення номера моделі сорочки
+  public String description = "Some description without any sense"; // стандартний опис сорочки
   // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
-  public char colorCode = 'U';
-  public double price = 0.0; // стандартна вартість сорочки
-  public int quantityInStock = 0; // стандартна кількість на складі
+  public String[] colors = {
+			"Red", 
+			"Blue",
+			"Green",
+			"Undefined",
+			};
+  int colorid = 2;
+  public double price = 20.2; // стандартна вартість сорочки
+  public int quantityInStock = 20; // стандартна кількість на складі
   
   // цей метод просто виводить всю інформацію про сорочку на екран
   public void displayShirtInformation() {
+    Scanner in = new Scanner(System.in);
     System.out.println("Shirt ID: " + shirtID);
     System.out.println("Shirt description:" + description);
-    System.out.println("Color Code: " + colorCode);
+    System.out.println("Color: " + colors[colorid]);
     System.out.println("Shirt price: $" + price);
     System.out.println("Quantity in stock: " + quantityInStock);
   } // кінець методу displayShirtInformation
